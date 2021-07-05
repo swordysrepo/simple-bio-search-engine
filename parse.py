12f8147a4e063a49efdf35d35352e0ead4b8e6d0
swordysrepo <kennethmcquadesales@gmail.com>
2021-07-04 23:44:11 -0400

    add readfile

---
+++ parse.py
@@ -0,0 +1,27 @@
+''' parse the titles.txt file into a json file'''
+
+'''
+parse each line , removing the parenthesis from the beginning and end .
+remove the quotes from beginning and end 
+remove comma from the end
+
+escape all characters
+
+preserve all inner parenthesis
+preserve all - characters
+store it as it is  , in raw format . 
+
+
+for each line , split the line into single characters and insert
+ to dictionary if unique
+export dictionary to json
+
+clear characters at start and end of line
+
+'''
+# file1 = open("titles.txt","r")
+# print(file1.readlines())
+import io
+with io.open("titles.txt", "r", encoding="utf-8") as my_file:
+     my_unicode_string = my_file.read() 
+     print(my_unicode_string)
